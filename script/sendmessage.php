33a14c66472bab7ed284c8e673eaebe5854dcc00
Samnang2k <Samnang2k24@outlook.com>
2024-09-19 00:09:19 +0700

    first commit

---
+++ script/sendmessage.php
@@ -0,0 +1,24 @@
+<?php
+    $content = '';
+    foreach($_POST as $key => $value){
+        $content .= "<b>$key</b>: <i>$value</i>\n";
+    }
+    if(trim($content)){
+        $content = "<b>Message from Site: </b>\n" .$content;
+        // bot token
+        $apiToken = "8155002295:AAGwo21L6VQ0XRrGKSW4Qj9rb116stUuYog";
+        $data = [
+            'chat_id' => '@MaSendMessageBots',
+            'text' => $content,
+            'parse_mode' => 'HTML',
+        ];
+        $response = file_get_content("https://api.telegram.org/bot$apiToken/sendMessage?".file_get_content($data));
+
+
+    }
+
+
+
+
+
+?>
